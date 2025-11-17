74b0e774d022e293d716f979b0b511568e8a4aad
Thiago M. <185578934+thiimont@users.noreply.github.com>
2025-11-16 22:12:49 -0300

    feat(repository): add findByUuidExterno method to TransacaoRepository

--- src/main/java/br/edu/fatecpg/soldi/repository/TransacaoRepository.java
+++ src/main/java/br/edu/fatecpg/soldi/repository/TransacaoRepository.java
@@ -7,19 +7,18 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
-
+    Optional<Transacao> findByUuidExterno(UUID uuidExterno);
 
     @Query("SELECT t FROM Transacao t WHERE t.usuario.uuid_externo = :uuidUsuario ORDER BY t.data_transacao DESC LIMIT 5")
-    List<Transacao> buscarUltimas5(@Param("uuidUsuario") UUID uuidUsuario);
-
+    List<Transacao> buscarUltimasCinco(@Param("uuidUsuario") UUID uuidUsuario);
 
     @Query("SELECT t FROM Transacao t WHERE t.usuario.uuid_externo = :uuidUsuario")
     List<Transacao> findAllByUsuarioUuid(@Param("uuidUsuario") UUID uuidUsuario);
-
 
     @Query("SELECT t FROM Transacao t WHERE t.usuario.uuid_externo = :uuidUsuario AND t.tipo = :tipo")
     List<Transacao> findByUsuarioUuidAndTipo(@Param("uuidUsuario") UUID uuidUsuario, @Param("tipo") String tipo);
