b553ceb09511feff9041e6c2809fe4d4adb24b49
Thiago M <185578934+thiimont@users.noreply.github.com>
2025-11-19 16:57:07 -0300

    feat(dto): modify DTOs to receive an enum instead of string

--- backend/src/main/java/br/edu/fatecpg/soldi/dto/response/TransacaoResumoDTO.java
+++ backend/src/main/java/br/edu/fatecpg/soldi/dto/response/TransacaoResumoDTO.java
@@ -1,7 +1,9 @@
 package br.edu.fatecpg.soldi.dto.response;
 
+import br.edu.fatecpg.soldi.model.TipoTransacao;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record TransacaoResumoDTO(UUID uuidTransacao, String tipo, BigDecimal valor, String descricao, String categoria, LocalDateTime dataTransacao) {}
+public record TransacaoResumoDTO(UUID uuidTransacao, TipoTransacao tipo, BigDecimal valor, String descricao, String categoria, LocalDateTime dataTransacao) {}
