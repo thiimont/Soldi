05c411eb794f65f45f7a13b6b63cb26f9ac77e23
Thiago M. <185578934+thiimont@users.noreply.github.com>
2025-11-21 23:59:09 -0300

    feat(config): add method to validate JWT tokens in TokenConfig

--- backend/src/main/java/br/edu/fatecpg/soldi/config/TokenConfig.java
+++ backend/src/main/java/br/edu/fatecpg/soldi/config/TokenConfig.java
@@ -3,10 +3,14 @@ package br.edu.fatecpg.soldi.config;
 import br.edu.fatecpg.soldi.model.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
 
 @Component
 public class TokenConfig {
@@ -22,5 +26,21 @@ public class TokenConfig {
                 .withExpiresAt(Instant.now().plusSeconds(86400))
                 .withIssuedAt(Instant.now())
                 .sign(algorithm);
+    }
+
+    public Optional<JWTUserData> validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            DecodedJWT decode = JWT.require(algorithm)
+                    .build().verify(token);
+
+            return Optional.of(JWTUserData.builder()
+                    .usuarioUuid(UUID.fromString(decode.getClaim("usuarioUuid").asString()))
+                    .email(decode.getSubject())
+                    .build());
+        } catch(JWTVerificationException ex) {
+            return Optional.empty();
+        }
+
     }
 }
