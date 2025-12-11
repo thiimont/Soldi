f4ac1f65675bab30c30d979ff6192050060df9bf
Thiago M. <185578934+thiimont@users.noreply.github.com>
2025-12-11 16:20:46 -0300

    fix(dto): change validations to NotBlank in LoginRequestDTO and AtualizarTransacaoDTO

--- backend/src/main/java/br/edu/fatecpg/soldi/dto/request/auth/LoginRequestDTO.java
+++ backend/src/main/java/br/edu/fatecpg/soldi/dto/request/auth/LoginRequestDTO.java
@@ -1,7 +1,7 @@
 package br.edu.fatecpg.soldi.dto.request.auth;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 
-public record LoginRequestDTO(@Email @NotEmpty(message = "O email é obrigatório.") String email,
-                              @NotEmpty(message = "A senha é obrigatória.") String senha) { }
+public record LoginRequestDTO(@Email @NotBlank(message = "O email é obrigatório.") String email,
+                              @NotBlank(message = "A senha é obrigatória.") String senha) { }
